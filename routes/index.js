251fc4171cbd4ef382ecf85ae43963d49008215f
Rick Walker <ricwalker59@gmail.com>
2017-06-09 16:23:56 -0400

    Modified non-authenticated view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const cfc = require('../modules/cfc');
 
 // home view
 router.get('/', (req, res) => {
-	res.send('home');
+	res.render('index', {title:cfc('home'), header:cfc('welcome - manage your profiles')});
+});
+
+// about view
+router.get('/about', (req, res) => {
+	res.render('about', {title:cfc('about'), header:cfc('who we are')});
+});
+
+// contact view
+router.get('/contact', (req, res) => {
+	res.render('contact', {title:cfc('contact'), header:cfc('how to contact us')});
 });
 
 module.exports = router;
